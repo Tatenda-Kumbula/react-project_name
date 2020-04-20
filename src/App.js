9049ca54fd883ab4e68ec2aef7406f715e236b20
Tatenda Kumbula <kumbula@603@gmail.com>
2020-04-20 22:36:42 +0200

    third commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
+
 import React, { Component } from 'react';
 import Products from './Products';
-
+import Rating from './Rating';
 
 class App extends Component {
-render() {
-    return (
-        <div>
-            <h1>My First React App!</h1>
-          <Products/>    
-        </div>
-        );
-    }
+  
+    render() {        
+  
+        return (
+      <div>
+        <Rating rating="1"/>
+        <Rating rating="2"/>
+        <Rating rating="3"/>
+        <Rating rating="4"/>
+        <Rating rating="5"/>      
+      </div>
+  
+      );
+  }
 }
+
 export default App;
